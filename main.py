fec746715a4cf389e0458213cd526f2a465cbc07
Alex Hollinghead <alexhollinghead@gmail.com>
2021-03-10 16:38:33 -0500

    Stripes now knows his name.

--- main.py
+++ main.py
@@ -18,4 +18,7 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send('Hello! I don\'t know how to do a whole lot yet, but I\'m happy to be here! :)')
 
+    if 'stripes' in message.content or 'Stripes' in message.content:
+        await message.add_reaction(u"\U0001F440")
+
 client.run(os.getenv('TOKEN'))
